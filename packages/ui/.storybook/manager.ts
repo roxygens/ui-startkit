903e85e64a7d0ae3dc7ef1d491d2c25eb622d8ab
jeffnts <jf.nts10@gmail.com>
2025-08-27 11:06:41 -0300

    style(manager.ts): remove unnecessary whitespace to improve code readability

--- packages/ui/.storybook/manager.ts
+++ packages/ui/.storybook/manager.ts
@@ -1,11 +1,10 @@
 // .storybook/manager.ts
 import { addons } from '@storybook/manager-api'
 import { create } from '@storybook/theming'
-import type { ThemeVars } from '@storybook/theming' // Importe ThemeVars para tipagem
+import type { ThemeVars } from '@storybook/theming'
 
 const myTheme: ThemeVars = create({
   base: 'dark',
-
   brandTitle: 'UI Start Kit - Components Docs',
   brandImage: '/images/logo.png',
   brandTarget: '_self',
