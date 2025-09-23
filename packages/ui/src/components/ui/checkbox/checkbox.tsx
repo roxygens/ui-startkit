df935bd4bcbd79e98b56e6ca625260f751cc0342
jeffnts <jf.nts10@gmail.com>
2025-09-23 14:05:16 -0300

    style(checkbox.tsx): remove unnecessary rounded corners from checkbox when description is present to simplify styling

--- packages/ui/src/components/ui/checkbox/checkbox.tsx
+++ packages/ui/src/components/ui/checkbox/checkbox.tsx
@@ -89,8 +89,7 @@ function Checkbox({ className, size, label, description, ...props }: Props) {
   return (
     <div
       className={cn('flex flex-row place-items-start gap-[0.5rem]', {
-        'border-1 border-neutral-gray rounded-[0.5rem] px-[0.75rem] min-w-[15rem] py-[0.5rem]':
-          description,
+        'border-1 border-neutral-gray px-[0.75rem] min-w-[15rem] py-[0.5rem]': description,
         'border-neutral-400': checked || props?.checked,
       })}
       onClick={handleContainerClick}
