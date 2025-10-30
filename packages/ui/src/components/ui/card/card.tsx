2419792d718618fe0ec94a8d7c582d7314b683a7
jeffnts <jf.nts10@gmail.com>
2025-10-30 17:03:38 -0300

    style(card.tsx): adjust z-index of Card footer to improve stacking context style(card.tsx): update background color class for CardList button for consistency and better hover effect

--- packages/ui/src/components/ui/card/card.tsx
+++ packages/ui/src/components/ui/card/card.tsx
@@ -191,7 +191,7 @@ Card.FooterButton = function CardFooterButton({
       <footer
         className={cn(
           `
-            lg:absolute lg:z-50 left-0  right-0 top-[100%]
+            lg:absolute lg:z-10 left-0  right-0 top-[100%]
             flex justify-between items-stretch 
             font-inter font-semibold text-xs leading-[1.13rem] 
             transform-gpu transition-transform duration-300 ease-in-out-translate-y-0
@@ -391,7 +391,7 @@ CardList.Button = function CardListButton({
     >
       <button
         className={cn(
-          'cursor-pointer rounded-none flex w-full items-center justify-center px-[1rem] h-[2.7rem] bg-primary/100  hover:bg-primary/70',
+          'cursor-pointer rounded-none flex w-full items-center justify-center px-[1rem] h-[2.7rem] bg-primary  hover:bg-primary-hover',
           {
             'bg-disabled text-disabled-foreground hover:bg-disabled hover:text-disabled-foreground':
               disabled,
